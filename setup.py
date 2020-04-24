893e80a2100beaecd0de4a193aa9a7bf584df16a
tjdodwell <t.dodwell@exeter.ac.uk>
2020-04-24 08:45:57 +0100

    changing process in setup

--- setup.py
+++ setup.py
@@ -1,21 +1,9 @@
 from setuptools import setup, find_packages
-#from Cython.Build import cythonize
+from Cython.Build import cythonize
 
 setup(
     name = "PETScGMsFEM",
     version="0.1",
     packages=find_packages(),
-    install_requires=[
-        'numpy',
-        'scipy',
-        'matplotlib',
-        'setuptools',
-        'cython',
-        'mpi4py',
-        'petsc4py',
-        'slepc4py',
-        'vtk',
-        'pytest'
-        ]
     #ext_modules = cythonize('GenEO/matelem_cython.pyx'),
 )
